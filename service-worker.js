937d67e8b9fb2a1e7407959c6d8c706055ab43fe
Ryan <bladelord542@outlook.com>
2026-02-16 14:11:59 -0800

    better title rendering

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'life-quest-journal-v185';
-const LAZY_CACHE_NAME = 'life-quest-journal-lazy-v185';
+const CACHE_NAME = 'life-quest-journal-v186';
+const LAZY_CACHE_NAME = 'life-quest-journal-lazy-v186';
 const urlsToCache = [
   './',
   './index.html',
