88cc75e511e79d646f3673cd63717527b81244b6
Ryan <bladelord542@outlook.com>
2026-03-16 15:59:31 -0700

    Added bright yellow to spotlight and pulse effect

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'life-quest-journal-v227';
-const LAZY_CACHE_NAME = 'life-quest-journal-lazy-v227';
+const CACHE_NAME = 'life-quest-journal-v228';
+const LAZY_CACHE_NAME = 'life-quest-journal-lazy-v228';
 // Local files: must all succeed or install fails (a missing local file = real bug)
 const localUrlsToCache = [
   './',
